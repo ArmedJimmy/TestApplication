80a3167681d4da78176ad26592f34e334507570f
Colin Robertson <colin24@live.co.uk>
2018-09-12 21:33:48 +0100

    Adding comment

--- TestApplication/Controllers/ValuesController.cs
+++ TestApplication/Controllers/ValuesController.cs
@@ -14,6 +14,7 @@ namespace TestApplication.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            //Adding a comment
             return new string[] { "value1", "value2" };
         }
 
